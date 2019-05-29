c27feed12a30c05e34078aea4f746ff669d789f1
Dustin Koch <dustin.koch@me.com>
2019-05-29 11:57:58 -0600

    word list created

---
+++ TableGames/Models/Word.swift
@@ -0,0 +1,40 @@
+//
+//  Word.swift
+//  TableGames
+//
+//  Created by Dustin Koch on 5/29/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import Foundation
+
+
+struct Word {
+    
+    let wordArray: [String] = ["ACID","AREA","ALOE","AGED","AQUA",
+                               "BARK","BALL","BOAT","BOOK","BULL",
+                               "CHEF","CALF","CHAT","CURE","CHIP",
+                               "DIVE","DIAL","DEAR","DARK","DUCK",
+                               "EACH","EVER","EPIC","EASY","EDIT",
+                               "FAME","FROG","FISH","FORK","FUEL",
+                               "GANG","GEEK","GOLF","GOAT","GATE",
+                               "HATE","HEAL","HERE","HOOK","HUNT",
+                               "ICED","ICON","IDEA","IRON","ITEM",
+                               "JAIL","JADE","JOCK","JOIN","JURY",
+                               "KALE","KEEP","KICK","KNOT","KISS",
+                               "LADY","LAWN","LILY","LOGO","LION",
+                               "MUTE","MADE","MESH","MEAN","MEAT",
+                               "NEED","NERD","NOTE","NAVY","NOON",
+                               "OATH","OINK","OVEN","ORCA","OUCH",
+                               "PILL","PATH","PULL","PLOT","PUNK",
+                               "QUIZ","QUIZ","QUAD",
+                               "RACE","ROOM","RIPE","RUSH","RAFT",
+                               "SAID","SAVE","SPOT","SWAN","SWIM",
+                               "TAXI","TACO","TOLL","TRAP","THEY",
+                               "UGLY","UNIT","USER","URGE","USED",
+                               "VIEW","VERY","VEIN","VAIL","VERB",
+                               "WAIT","WILD","WORK","WHIP","WOOL",
+                               "XRAY",
+                               "YARD","YARN","YEAR","YOGA","YOUR",
+                               "ZERO","ZOOM","ZINC","ZANY"]
+} //END OF STRUCT
