9e166bfb03c77defe11c2217615102b9c186007c
Dustin Koch <dustin.koch@me.com>
2019-05-29 09:53:43 -0600

    storyboards for initial navigation complete

---
+++ TableGames/ViewControllers/ChooseGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChooseGameViewController.swift
+//  TableGames
+//
+//  Created by Dustin Koch on 5/29/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import UIKit
+
+class ChooseGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: false)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
