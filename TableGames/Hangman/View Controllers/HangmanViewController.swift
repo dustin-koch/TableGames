8931861f65a81a5c85b5a2c368c480e7c1a76fe4
Dustin Koch <dustin.koch@me.com>
2019-05-30 10:23:02 -0600

    constraints added for hangman game

--- TableGames/Hangman/View Controllers/HangmanViewController.swift
+++ TableGames/Hangman/View Controllers/HangmanViewController.swift
@@ -19,7 +19,8 @@ class HangmanViewController: UIViewController {
     @IBOutlet weak var secondLetterLabel: UILabel!
     @IBOutlet weak var thirdLetterLabel: UILabel!
     @IBOutlet weak var fourthLetterLabel: UILabel!
-    @IBOutlet weak var dashesStackView: UIStackView!
+    
+    
     
     //MARK: - Properties
     var word = ""
@@ -28,21 +29,38 @@ class HangmanViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        clearScreen()
+        clearLetters()
         drawAndRemoveNewWord()
         print(word)
     }
     
     //MARK: - Actions
     @IBAction func resetGameButtonTapped(_ sender: Any) {
-        clearScreen()
+        clearLetters()
         drawAndRemoveNewWord()
+        updateJailImage()
+        //refresh try counter and opacity of jail
         print(word)
     }
+    @IBAction func aTapped(_ sender: UIButton) {
+        //see if letter is in the word (guessed helper func)
+        
+    }
+    @IBAction func bTapped(_ sender: UIButton) {
+    }
+    @IBAction func cTapped(_ sender: UIButton) {
+    }
+    @IBAction func dTapped(_ sender: UIButton) {
+    }
+    @IBAction func eTapped(_ sender: UIButton) {
+    }
+    //Finish adding actions here
+
+    
     
     
     //MARK: - Helper Functions
-    func clearScreen() {
+    func clearLetters() {
         firstLetterLabel.text = ""
         secondLetterLabel.text = ""
         thirdLetterLabel.text = ""
@@ -55,6 +73,56 @@ class HangmanViewController: UIViewController {
         count = 5
     }
     
-
+    func updateJailImage() {
+        let opacity = jailOpacity
+        jailImageView.image = UIImage(named: "jail")?.alpha(CGFloat(opacity))
+    }
+    
+    func guessed(letter: Character, forWord: String) {
+        //for loop to check/repeat this?
+        if forWord.contains(letter) {
+            //stuff to do if guess letter correctly
+            //update label for correct letter guessed
+            
+            jailOpacity -= 0.25
+            updateJailImage()
+            
+            guard let range: Range<String.Index> = forWord.range(of: String(letter)) else { return }
+            let index: Int = forWord.distance(from: forWord.startIndex, to: range.lowerBound)
+            
+            switch index {
+            case 0:
+                print("insert here")
+            case 1:
+                print("insert here")
+            case 2:
+                print("insert here")
+            case 3:
+                print("insert here")
+            default:
+                print("Error here 🍋")
+            }
+            
+            
+            
+        } else {
+            //stuff to do if letter is incorrect
+            count -= 1
+            let status = Float((count - 5) / 5).magnitude
+            progressBar.setProgress(status, animated: true)
+            countdownValueLabel.text = String(count)
+        }
+    }
 
 }//END OF CLASS
+
+//Extension to change opacity of images
+extension UIImage {
+    func alpha(_ value:CGFloat) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: CGPoint.zero, blendMode: .normal, alpha: value)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+}
